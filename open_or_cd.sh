b9ba73db308c312b72d4c8bbee6bb3c45da2a184
Isaac Renner <isaac@ailo.io>
2023-04-08 14:01:25 +1000

    update open_or_cd to be used as a zsh function

--- open_or_cd.sh
+++ open_or_cd.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
+open_or_cd() {
+  if [ -z "$1" ]; then
+    echo "Usage: open_or_cd <path>"
+    return 1
+  fi
 
-# Check if the argument is provided
+  path="$1"
+
+  if [ ! -e "$path" ]; then
+    echo "Error: Path not found"
+    return 1
+  fi
+
+  if [ -d "$path" ]; then
+    cd "$path" && echo "Changed directory to: $(pwd)"
+  else
+    vim "$path"
+  fi
+}
+
+# If the script is sourced as a Zsh function, execute the function
+if [ "$IS_ZSH_FUNCTION" = "1" ]; then
+  open_or_cd "$@"
+  return 0
+fi
+
+# The script continues to execute as a standalone shell script
 if [ -z "$1" ]; then
-  echo "Usage: ./open_or_cd.sh <path>"
+  echo "Usage: $0 <path>"
   exit 1
 fi
 
 path="$1"
 
-# Check if the path exists
 if [ ! -e "$path" ]; then
   echo "Error: Path not found"
   exit 1
 fi
 
-# Check if it's a directory
 if [ -d "$path" ]; then
   cd "$path" && echo "Changed directory to: $(pwd)"
-  # Execute a new shell to keep the user in the new directory
   $SHELL
 else
   vim "$path"
