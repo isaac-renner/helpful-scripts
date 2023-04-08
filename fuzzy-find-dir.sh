50c762fac320466051cb1734681a63eaa8efafc8
Isaac Renner <isaac@ailo.io>
2023-04-08 14:12:39 +1000

    convert scripts to be sourced by zsh scripts

--- fuzzy-find-dir.sh
+++ fuzzy-find-dir.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+select_dir() {
+  DEFAULT_PATH=$(echo ~)
+  PATH_TO_DIR=${1:=$DEFAULT_PATH}
 
-DEFAULT_PATH=$(echo ~)
-PATH_TO_DIR=${1:=$DEFAULT_PATH}
+  # Check if fzf is installed
+  if ! command -v fzf &> /dev/null; then
+    echo "Error: fzf is not installed or not in the system's PATH"
+    return 1
+  fi
 
-# Check if fzf is installed
-if ! command -v fzf &> /dev/null; then
-  echo "Error: fzf is not installed or not in the system's PATH"
-  exit 1
+  # Get list of dirs at path
+  SELECTED_DIR=$(cd "$PATH_TO_DIR" && ls -d * | fzf)
+
+  if [[ "$SELECTED_DIR" == "" ]]; then
+    return 0
+  fi
+
+  echo "${PATH_TO_DIR}/${SELECTED_DIR}"
+}
+
+# If the script is sourced as a Zsh function, do not execute the function
+if [ "$0" != "${BASH_SOURCE[0]}" ]; then
+  return 0
 fi
 
-# get list of dirs at path
-SELECTED_DIR=$(cd $PATH_TO_DIR && ls -d * | fzf) 
-
-if [[ "$SELECTED_DIR" == "" ]]; then
-  exit 0
-fi
-
-echo "${PATH_TO_DIR}/${SELECTED_DIR}"
+select_dir "$@"
--- open_or_cd.sh
+++ open_or_cd.sh
@@ -1,46 +1,27 @@
 #!/bin/bash
 open_or_cd() {
   if [ -z "$1" ]; then
-    echo "Usage: open_or_cd <path>"
+    echo "Usage: open_or_cd <path_to_change_to>"
     return 1
   fi
 
-  path="$1"
+  path_to_change_to="$1"
 
-  if [ ! -e "$path" ]; then
-    echo "Error: Path not found"
+  if [ ! -e "$path_to_change_to" ]; then
+    echo "Error: path_to_change_to not found"
     return 1
   fi
 
-  if [ -d "$path" ]; then
-    cd "$path" && echo "Changed directory to: $(pwd)"
+  if [ -d "$path_to_change_to" ]; then
+    cd "$path_to_change_to" && echo "Changed directory to: $(pwd)"
   else
-    vim "$path"
+    vim "$path_to_change_to"
   fi
 }
 
-# If the script is sourced as a Zsh function, execute the function
-if [ "$IS_ZSH_FUNCTION" = "1" ]; then
-  open_or_cd "$@"
+# If the script is sourced as a Zsh function, do not execute the function
+if [ "$0" != "${BASH_SOURCE[0]}" ]; then
   return 0
 fi
 
-# The script continues to execute as a standalone shell script
-if [ -z "$1" ]; then
-  echo "Usage: $0 <path>"
-  exit 1
-fi
-
-path="$1"
-
-if [ ! -e "$path" ]; then
-  echo "Error: Path not found"
-  exit 1
-fi
-
-if [ -d "$path" ]; then
-  cd "$path" && echo "Changed directory to: $(pwd)"
-  $SHELL
-else
-  vim "$path"
-fi
+open_or_cd "$@"
